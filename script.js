e8f4fc9ab1fd9c8074d99f081cbeea6d8f0249e3
marzdollar <marzdollar@gmail.com>
2026-09-12 01:34:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function sendMessage() {
 
     let name = document.getElementById("name").value;
- let email = document.getElementById("email").value; 
-    
+    let email = document.getElementById("email").value;
+
     if (name === "") {
         alert("Please enter your name.");
+    } else if (email === "") {
+        alert("Please enter your email.");
     } else {
         alert("Welcome " + name + "! Your message was received.");
     }
 
 }
-
