588755a3baccbb3e7e1bbbe7ede236371d3503e2
marzdollar <marzdollar@gmail.com>
2026-09-12 01:39:40 +0300

    Add email validation in sendMessage function

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function sendMessage() {
 
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-
+if (!email.includes("@")) {
+    alert("Please enter a valid email.");
+    return;
+}    
     if (name === "") {
         alert("Please enter your name.");
     } else if (email === "") {
